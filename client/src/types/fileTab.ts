b27d0c4da78700aeba7214c1d9bf00d8f40779ce
tejaskhile <tejaskhile07@gmail.com>
2025-08-22 22:37:34 +0530

    Updates

--- client/src/types/fileTab.ts
+++
@@ -1,9 +0,0 @@
-import { FileSystemItem } from "./file"
-
-interface FileTabContext {
-    activeFile: FileSystemItem | null
-    setActiveFile: (file: FileSystemItem) => void
-    changeActiveFile: (fileId: string) => void
-}
-
-export { FileTabContext }
--- client/src/utils/customMapping.ts
+++
@@ -1,7 +0,0 @@
-const customMapping: { [key: string]: string } = {
-    php: "php",
-    cs: "csharp",
-    // Maybe add more languages here
-}
-
-export default customMapping
